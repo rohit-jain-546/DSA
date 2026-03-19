e800453a46a16c6a1a7d25613560a0be0474e96e
rohit-jain-546 <rohitjain34793@gmail.com>
2026-03-19 23:19:08 +0530

    13

---
+++ LeetCode/13.romanToInt.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+        
+        int n = 0;
+        int sum = 0;
+        for(int i=s.length()-1; i>=0; i--){
+            switch (s.charAt(i)) {
+                case 'I':
+                    n = 1;
+                    break;
+                case 'V':
+                    n = 5;
+                    break;
+                case 'X':
+                    n = 10;
+                    break;
+                case 'L':
+                    n = 50;
+                    break;
+                case 'C':
+                    n = 100;
+                    break;
+                case 'D':
+                    n = 500;
+                    break;
+                case 'M':
+                    n = 1000;
+                    break;
+            }
+            if(4*n<sum)
+            sum -= n;
+            else
+            sum += n;
+        } 
+        return sum;   
+    
+    
+    }
+}
