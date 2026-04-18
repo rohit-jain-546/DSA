b96edb685b9a3580d2d7abdc60c4f0057ad1e276
rohit-jain-546 <rohitjain34793@gmail.com>
2026-04-18 20:37:13 +0530

    162

---
+++ LeetCode/162. find peak element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int s=0, e= arr.length-1;
+       while(s<e){
+        int mid = s+(e-s)/2;
+        if(arr[mid]>arr[mid+1]){
+            e=mid;
+        }else s=mid+1;
+       }return s;
+    }
+}
