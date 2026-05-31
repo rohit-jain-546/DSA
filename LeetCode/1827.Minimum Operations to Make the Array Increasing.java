b60cc59291f86429011a4bf077580184960710a9
rohit-jain-546 <rohitjain34793@gmail.com>
2026-05-31 14:29:03 +0530

    1827

---
+++ LeetCode/1827.Minimum Operations to Make the Array Increasing.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans = 0;
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] <= nums[i - 1]) {
+                int diff = nums[i - 1] - nums[i] + 1;
+                ans += diff;
+                nums[i] += diff;
+            }
+        }
+
+        return ans;
+    }
+}
