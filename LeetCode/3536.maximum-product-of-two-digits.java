0b1e4f0bb85290941cdde364ec5432e160034620
rohit-jain-546 <rohitjain34793@gmail.com>
2026-07-25 07:20:26 +0530

    3536

---
+++ LeetCode/3536.maximum-product-of-two-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int n) {
+        int a = -1, b = -1;
+        while (n > 0) {
+            int d = n % 10;
+            if (d >= a) {
+                b = a;
+                a = d;
+            } else if (d > b) {
+                b = d;
+            }
+            n /= 10;
+        }
+        return a * b;
+    }
+}
